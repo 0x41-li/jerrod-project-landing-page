ebfb8908f3de473f4db34d2a8757d8c874bc0ffc
Ali Sbane <ali.sbane.dev@gmail.com>
2022-11-03 01:39:38 +0100

    Made some modifications to many parts

--- js/app.js
+++ js/app.js
@@ -2,6 +2,6 @@ const humIcon = document.querySelector(".header__hum-icon");
 const nav = document.querySelector(".header__nav");
 
 humIcon.addEventListener("click", () => {
-    humIcon.classList.toggle("show");
-    nav.classList.toggle("show");
+  humIcon.classList.toggle("show");
+  nav.classList.toggle("show");
 });
